02cdd42f146f1ec17b4f05e351cdc97cee631fec
toshmosh <todorjelev36@gmail.com>
2014-08-20 21:15:43 +0300

    -update ignore list

--- FableWorld/Main.cpp
+++ FableWorld/Main.cpp
@@ -15,8 +15,10 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance,PSTR cmdLine, int
 
 	fout.open("logs/log.txt");
 
-	pEngine = new GameEngine(hInstance, "Fable World", D3DDEVTYPE_HAL, D3DCREATE_HARDWARE_VERTEXPROCESSING,true);
+	pEngine = new GameEngine(hInstance, "Fable World", D3DDEVTYPE_HAL, D3DCREATE_HARDWARE_VERTEXPROCESSING);
 	
+	m_pGameObjManager = new GameObjectManager(true,false);
+
 	//adds 3 scenes in the scenesContainer and set the active scene to be the menu scene.
 	Menu menu;
 	pEngine->AddScene("menu",&menu);
