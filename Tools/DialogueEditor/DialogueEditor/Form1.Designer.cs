30cd0d68c9fa4e9c39a4e790d313fc9ed2c522ba
todorjelev36@gmail.com <todorjelev36@gmail.com>
2015-12-31 15:48:01 +0200

    + added dialogue editor, needed for the dialogues in the game

---
+++ Tools/DialogueEditor/DialogueEditor/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace DialogueEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("You have to...");
+            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("You dont have to");
+            System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("Hello", new System.Windows.Forms.TreeNode[] {
+            treeNode6,
+            treeNode7});
+            System.Windows.Forms.TreeNode treeNode9 = new System.Windows.Forms.TreeNode("goodbye");
+            System.Windows.Forms.TreeNode treeNode10 = new System.Windows.Forms.TreeNode("RootNode", new System.Windows.Forms.TreeNode[] {
+            treeNode8,
+            treeNode9});
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.AddNode = new System.Windows.Forms.Button();
+            this.RemoveNode = new System.Windows.Forms.Button();
+            this.ExpandAll = new System.Windows.Forms.Button();
+            this.inputText = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.treeView1.Location = new System.Drawing.Point(131, 39);
+            this.treeView1.Name = "treeView1";
+            treeNode6.Name = "Node3";
+            treeNode6.Text = "You have to...";
+            treeNode7.Name = "Node4";
+            treeNode7.Text = "You dont have to";
+            treeNode8.Name = "Hello";
+            treeNode8.Text = "Hello";
+            treeNode9.Name = "Node2";
+            treeNode9.Text = "goodbye";
+            treeNode10.Name = "RootNode";
+            treeNode10.Tag = "Root";
+            treeNode10.Text = "RootNode";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode10});
+            this.treeView1.Size = new System.Drawing.Size(979, 435);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1122, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.quitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.newToolStripMenuItem.Text = "New Dialogue";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.openToolStripMenuItem.Text = "Import Dialogue";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.saveToolStripMenuItem.Text = "Eport Dialogue";
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.quitToolStripMenuItem.Text = "Quit";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.helpToolStripMenuItem1,
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // helpToolStripMenuItem1
+            // 
+            this.helpToolStripMenuItem1.Name = "helpToolStripMenuItem1";
+            this.helpToolStripMenuItem1.Size = new System.Drawing.Size(107, 22);
+            this.helpToolStripMenuItem1.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // AddNode
+            // 
+            this.AddNode.Location = new System.Drawing.Point(12, 39);
+            this.AddNode.Name = "AddNode";
+            this.AddNode.Size = new System.Drawing.Size(103, 30);
+            this.AddNode.TabIndex = 2;
+            this.AddNode.Text = "Add Node";
+            this.AddNode.UseVisualStyleBackColor = true;
+            this.AddNode.Click += new System.EventHandler(this.AddChildNode_Click);
+            // 
+            // RemoveNode
+            // 
+            this.RemoveNode.Location = new System.Drawing.Point(12, 75);
+            this.RemoveNode.Name = "RemoveNode";
+            this.RemoveNode.Size = new System.Drawing.Size(103, 30);
+            this.RemoveNode.TabIndex = 4;
+            this.RemoveNode.Text = "Remove Node";
+            this.RemoveNode.UseVisualStyleBackColor = true;
+            this.RemoveNode.Click += new System.EventHandler(this.RemoveNode_Click);
+            // 
+            // ExpandAll
+            // 
+            this.ExpandAll.Location = new System.Drawing.Point(12, 111);
+            this.ExpandAll.Name = "ExpandAll";
+            this.ExpandAll.Size = new System.Drawing.Size(103, 30);
+            this.ExpandAll.TabIndex = 5;
+            this.ExpandAll.Text = "Expand Node";
+            this.ExpandAll.UseVisualStyleBackColor = true;
+            this.ExpandAll.Click += new System.EventHandler(this.ExpandAll_Click);
+            // 
+            // inputText
+            // 
+            this.inputText.AcceptsReturn = true;
+            this.inputText.AcceptsTab = true;
+            this.inputText.Location = new System.Drawing.Point(131, 500);
+            this.inputText.Name = "inputText";
+            this.inputText.Size = new System.Drawing.Size(979, 20);
+            this.inputText.TabIndex = 6;
+            this.inputText.TextChanged += new System.EventHandler(this.NodeText_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1122, 589);
+            this.Controls.Add(this.inputText);
+            this.Controls.Add(this.ExpandAll);
+            this.Controls.Add(this.RemoveNode);
+            this.Controls.Add(this.AddNode);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Button AddNode;
+        private System.Windows.Forms.Button RemoveNode;
+        private System.Windows.Forms.Button ExpandAll;
+        private System.Windows.Forms.TextBox inputText;
+    }
+}
+
---
+++ Tools/DialogueEditor/DialogueEditor/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DialogueEditor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            inputText.KeyDown += new KeyEventHandler(NodeText_KeyDown);
+            //inputText.KeyDown += new KeyEventArgs(NodeText_KeyDown);
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            selectedTreeNode = e.Node;
+
+            inputText.Text = selectedTreeNode.Text;
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            treeView1.Nodes.Clear();
+            treeView1.Nodes.Add("Root");
+            treeView1.SelectedNode = treeView1.Nodes[0];
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            treeView1.Nodes.Add("OMG BRAT");
+            treeView1.Nodes[1].Nodes.Add("ebasi");
+        }
+
+        private void RemoveNode_Click(object sender, EventArgs e)
+        {
+            if(treeView1.SelectedNode != null )
+            {
+                treeView1.SelectedNode.Remove();
+            }
+        }
+
+        private void AddChildNode_Click(object sender, EventArgs e)
+        {
+            if( treeView1.Nodes.Count == 0)
+            {
+                treeView1.Nodes.Add("Root");
+                treeView1.SelectedNode = treeView1.Nodes[0];
+            }
+            else
+            {
+                treeView1.SelectedNode.Nodes.Add("test");
+                treeView1.SelectedNode.Expand();
+            }
+        }
+
+        private void ExpandAll_Click(object sender, EventArgs e)
+        {
+            if(treeView1.SelectedNode != null)
+            {
+                treeView1.SelectedNode.ExpandAll();
+            }
+        }
+
+        private void NodeText_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void NodeText_KeyDown(object sender, KeyEventArgs e)
+        {
+            var character = e.KeyData;
+
+            if (e.KeyCode == Keys.Enter)
+            {
+                selectedTreeNode.Text = inputText.Text;
+            }
+            else
+            {
+                inputText.Text.Insert(inputText.Text.Length, e.KeyData.ToString());
+            }
+        }
+
+        public TreeNode selectedTreeNode;
+
+        private void retrieveInput_Click(object sender, EventArgs e)
+        {
+            selectedTreeNode.Text = inputText.Text;
+        }
+    }
+}
